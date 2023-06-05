227ff0a40f11ab03e5f808f0059f6b3d4eb089c2
Matheus Gomes <matheus.oliveira@rubcube.com>
2023-06-05 13:54:12 -0300

    project init

---
+++ rubcamp-frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './pages/Main.tsx'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+)
---
+++ rubcamp-frontend/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function Main() {
+
+  return (
+    <div>TESTE</div>
+  )
+}
+
+export default Main
